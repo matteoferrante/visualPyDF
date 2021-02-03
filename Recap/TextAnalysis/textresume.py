a0292dddb80b5a8d54ade4980dbb7ba5f959bfa3
matteo <matteo-ferrante@hotmail.it>
2021-02-03 10:47:21 +0100

    nltk download

--- Recap/TextAnalysis/textresume.py
+++ Recap/TextAnalysis/textresume.py
@@ -10,8 +10,6 @@ import progressbar
 from nltk.cluster.util import cosine_distance
 import networkx as nx
 
-nltk.download('punkt')
-
 class TextResume:
     def __init__(self,text,method):
         """
